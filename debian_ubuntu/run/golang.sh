9fafe134dfca7acda8c4810bdd21ca1b3fd71599
Salvydas Lukosius <sall@w-ss.io>
2020-12-07 10:38:55 +0000

    test

--- debian_ubuntu/run/golang.sh
+++ debian_ubuntu/run/golang.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+SRCDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "${SRCDIR}/utils.sh"
+
 # GoLang
 ##########################################################
 installGoLang() {
