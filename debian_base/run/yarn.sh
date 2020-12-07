3306db41124ce02ce81bfd4fc82e51d8ba4dc61c
Salvydas Lukosius <sall@w-ss.io>
2020-12-07 12:40:00 +0000

    test

--- debian_base/run/yarn.sh
+++ debian_base/run/yarn.sh
@@ -1,8 +1,14 @@
+#!/usr/bin/env bash
+SRCDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "${SRCDIR}/utils.sh"
 
 # Yarn
 ##########################################################
 installYarn() {
+    title "Adding repository"
+    repoYarn
     title "Installing Yarn"
     sudo apt install -y yarn
     breakLine
 }
+installYarn
