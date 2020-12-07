379de982c53f618a6f2f800906df485859cc21fe
Salvydas Lukosius <sall@w-ss.io>
2020-12-07 11:53:59 +0000

    test

--- debian_ubuntu/run/node.sh
+++ debian_ubuntu/run/node.sh
@@ -6,7 +6,7 @@ source "${SRCDIR}/utils.sh"
 installNode() {
     title "Installing Node ${versionNode}"
     curl -L "https://deb.nodesource.com/setup_${versionNode}.x" | sudo -E bash -
-    sudo apt install -y nodejs npm
+    sudo apt install -y nodejs node-gyp npm
 
     if [[ ${versionDeb} = "stretch" ]]; then
         sudo chown -R "$(whoami)" /usr/lib/node_modules
