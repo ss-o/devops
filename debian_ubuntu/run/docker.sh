6652cfe15ebd2306f4bce138e23d35ba03906ad9
Salvydas Lukosius <sall@w-ss.io>
2020-12-07 10:15:05 +0000

    new file:   debian_ubuntu/run/docker-docker-compose.sh 	modified:   debian_ubuntu/run/docker.sh

--- debian_ubuntu/run/docker.sh
+++ debian_ubuntu/run/docker.sh
@@ -5,47 +5,13 @@ source "${SRCDIR}/utils.sh"
 # Docker
 ##########################################################
 installDocker() {
-    title "Add repository"
-    repoDocker
-    title "Installing Docker CE with Docker Compose"
-    sudo apt install -y curl docker-ce
-    curlToFile "https://github.com/docker/compose/releases/download/${versionDockerCompose}/docker-compose-$(uname -s)-$(uname -m)" "/usr/local/bin/docker-compose"
-    sudo chmod +x /usr/local/bin/docker-compose
-
-    sudo groupadd docker
-    sudo usermod -aG docker "${USER}"
-
-    notify "Install a separate runc environment? (recommended on chromebooks)"
-
-    if _confirm; then
-
-            if [[ ${installedGo} -ne 1 ]] && [[ "$(command -v go)" == '' ]]; then
-                breakLine
-                installGoLang
-            fi
-
-            sudo sed -i -e 's/ExecStartPre=\/sbin\/modprobe overlay/#ExecStartPre=\/sbin\/modprobe overlay/g' /lib/systemd/system/containerd.service
-
-            sudo apt install libseccomp-dev -y
-            go get -v github.com/opencontainers/runc
-            go get -v github.com/opencontainers/runc
-
-            cd "${GOPATH}/src/github.com/opencontainers/runc" || exit
-            make BUILDTAGS='seccomp apparmor'
-
-            sudo cp "${GOPATH}/src/github.com/opencontainers/runc/runc" /usr/local/bin/runc-master
-
-            curlToFile "${repoUrl}docker/daemon.json" /etc/docker/daemon.json
-            sudo systemctl daemon-reload
-            sudo systemctl restart containerd.service
-            sudo systemctl restart docker
-            break
-        else
-        break
-    fi
-
-    breakLine
+    title "Installing Docker"
+    echo "Installing docker"
+    execute wget get.docker.com -O dockerInstall.sh
+    execute chmod +x dockerInstall.sh
+    execute ./dockerInstall.sh
+    execute rm dockerInstall.sh
+    # Adds user to the `docker` group so that docker commands can be run without sudo
+    execute sudo usermod -aG docker ${USER}
 }
-
 installDocker
-
