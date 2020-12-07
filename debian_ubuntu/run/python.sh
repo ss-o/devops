8423a5054de838ed3390c9f0ca296aff88957b12
Salvydas Lukosius <sall@w-ss.io>
2020-12-07 12:05:06 +0000

    test

--- debian_ubuntu/run/python.sh
+++ debian_ubuntu/run/python.sh
@@ -6,6 +6,7 @@ source "${SRCDIR}/utils.sh"
 # Python
 ##########################################################
 installPython() {
+
     title "Installing Python3 with PIP"
     sudo apt install -y build-essential libssl-dev libffi-dev python-dev python3-pip
     sudo ln -s /usr/bin/pip3 /usr/bin/pip
