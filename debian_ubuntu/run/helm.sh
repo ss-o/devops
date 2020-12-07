928757af3f74d29d1d9ec74064ba0aecc746f8fc
Salvydas Lukosius <sall@w-ss.io>
2020-12-07 11:29:02 +0000

    test

--- debian_ubuntu/run/helm.sh
+++ debian_ubuntu/run/helm.sh
@@ -11,17 +11,17 @@ installHelm() {
     case "$(uname -m)" in
         aarch64) curl -fsSl "https://get.helm.sh/helm-v${versionHelm}-linux-arm64.tar.gz" -o helm.tar.gz
                 tar -zxvf helm.tar.gz
-                sudo mv linux-arm64/helm /usr/local/bin/helm
+                sudo mv linux-arm64/helm /usr/local/bin/helm || echo "Already exists"
                 sudo rm -rf linux-arm64 && sudo rm helm.tar.gz
                 ;; 
         x86_64) curl -fsSl "https://get.helm.sh/helm-v${versionHelm}-linux-amd64.tar.gz" -o helm.tar.gz
                 tar -zxvf helm.tar.gz
-                sudo mv linux-amd64/helm /usr/local/bin/helm
+                sudo mv linux-amd64/helm /usr/local/bin/helm || echo "Already exists"
                 sudo rm -rf linux-amd64 && sudo rm helm.tar.gz
                 ;;
         armv7l)  curl -fsSl "https://get.helm.sh/helm-v${versionHelm}-linux-arm.tar.gz" -o helm.tar.gz
                 tar -zxvf helm.tar.gz
-                sudo mv linux-arm/helm /usr/local/bin/helm
+                sudo mv linux-arm/helm /usr/local/bin/helm || echo "Already exists"
                 sudo rm -rf linux-arm && sudo rm helm.tar.gz
                 ;;
         *)      echo "Not found"
