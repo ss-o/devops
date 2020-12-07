54793c5fb69c528454c63e0eb005dbed330b7af6
Salvydas Lukosius <sall@w-ss.io>
2020-12-07 11:19:31 +0000

    test

--- debian_ubuntu/run/helm.sh
+++ debian_ubuntu/run/helm.sh
@@ -5,22 +5,26 @@ source "${SRCDIR}/utils.sh"
 # Helm
 ##########################################################
 installHelm() {
-    title "Installing Helm v${versionHelm}"
-        getarch="uname -m"
     
+    title "Installing Helm v${versionHelm}"    
+    getarch="uname -m"
+
     case "$geterch" in
-        aarch64)curl -fsSl "https://get.helm.sh/helm-v${versionHelm}-linux-arm64.tar.gz" -o helm.tar.gz
+        aarch64) curl -fsSl "https://get.helm.sh/helm-v${versionHelm}-linux-arm64.tar.gz" -o helm.tar.gz
                 tar -zxvf helm.tar.gz
                 sudo mv linux-arm64/helm /usr/local/bin/helm
-                sudo rm -rf linux-arm64 && sudo rm helm.tar.gz  ;; 
-        x86_64)  curl -fsSl "https://get.helm.sh/helm-v${versionHelm}-linux-amd64.tar.gz" -o helm.tar.gz
+                sudo rm -rf linux-arm64 && sudo rm helm.tar.gz
+                ;; 
+        x86_64) curl -fsSl "https://get.helm.sh/helm-v${versionHelm}-linux-amd64.tar.gz" -o helm.tar.gz
                 tar -zxvf helm.tar.gz
                 sudo mv linux-amd64/helm /usr/local/bin/helm
-                sudo rm -rf linux-amd64 && sudo rm helm.tar.gz ;;
+                sudo rm -rf linux-amd64 && sudo rm helm.tar.gz
+                ;;
         armv7l)  curl -fsSl "https://get.helm.sh/helm-v${versionHelm}-linux-arm.tar.gz" -o helm.tar.gz
                 tar -zxvf helm.tar.gz
                 sudo mv linux-arm/helm /usr/local/bin/helm
-                sudo rm -rf linux-arm && sudo rm helm.tar.gz ;;
+                sudo rm -rf linux-arm && sudo rm helm.tar.gz
+                ;;
     esac
     breakLine
 }
