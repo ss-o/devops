b9a612ecd8f152161745f6f9727bc2370953c108
Salvydas Lukosius <sall@w-ss.io>
2020-12-12 20:02:34 +0000

    test

--- python-tools.sh
+++ python-tools.sh
@@ -5,75 +5,75 @@ source "${CDIR}/utils.sh"
 title "Installing Python, PIP & tools"
 
 #if ! _cmd_ python; then
-    if _exec_ apt; then
-        sudo apt install -y build-essential libssl-dev libffi-dev  python3-venv python-tk python-dev python3-pip
-    fi
-    if _exec_ pacman; then
-        sudo pacman -S python python-virtualenv python-pipenv python-pip --noconfirm
-    fi
+if _exec_ apt; then
+	sudo apt install -y build-essential libssl-dev libffi-dev python3-venv python-tk python-dev python3-pip
+fi
+if _exec_ pacman; then
+	sudo pacman -S python python-virtualenv python-pipenv python-pip --noconfirm
+fi
 #fi
 
 mkdir -p ${HOME}/.local/bin
 source ~/.bashrc
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 ${PWD}/get-pip.py --user
-	pip install --user --upgrade pip
-	pip install --user ansible
-	pip install --user ansible-lint
-	pip install --user autopep8
-	pip install --user black
-	pip install --user cheat
-	pip install --user chromedriver-binary
-	pip install --user diagrams
-	pip install --user django
-	pip install --user faker
-	pip install --user flake8
-	pip install --user gif-for-cli
-	pip install --user graph-cli
-	pip install --user httpie
-	pip install --user importmagic
-	pip install --user ipywidgets
-	pip install --user jedi
-	pip install --user jupyter
-	pip install --user jupyterlab
-	pip install --user jupyterthemes
-	pip install --user litecli
-	pip install --user matplotlib
-	pip install --user neovim
-	pip install --user nose
-	pip install --user pandas
-	pip install --user pipenv
-	pip install --user poetry
-	pip install --user progressbar2
-	pip install --user psycopg2-binary
-	pip install --user py-spy
-	pip install --user pydoc_utils
-	pip install --user pyflakes
-	pip install --user pylint
-	pip install --user python-language-server
-	pip install --user r7insight_python
-	pip install --user redis
-	pip install --user requests_mock
-	pip install --user rope
-	pip install --user rtv
-	pip install --user scipy
-	pip install --user scrapy
-	pip install --user seaborn
-	pip install --user selenium
-	pip install --user speedtest-cli
-	pip install --user streamlink
-	pip install --user tldr
-	pip install --user trash-cli
-        pip install --user pygments
-     	pip install --user truffleHog
-	pip install --user virtualenv
-	pip install --user virtualenvwrapper
-	pip install --user yapf
+pip install --user --upgrade pip
+pip install --user ansible
+pip install --user ansible-lint
+pip install --user autopep8
+pip install --user black
+pip install --user cheat
+pip install --user chromedriver-binary
+pip install --user diagrams
+pip install --user django
+pip install --user faker
+pip install --user flake8
+pip install --user gif-for-cli
+pip install --user graph-cli
+pip install --user httpie
+pip install --user importmagic
+pip install --user ipywidgets
+pip install --user jedi
+pip install --user jupyter
+pip install --user jupyterlab
+pip install --user jupyterthemes
+pip install --user litecli
+pip install --user matplotlib
+pip install --user neovim
+pip install --user nose
+pip install --user pandas
+pip install --user pipenv
+pip install --user poetry
+pip install --user progressbar2
+pip install --user psycopg2-binary
+pip install --user py-spy
+pip install --user pydoc_utils
+pip install --user pyflakes
+pip install --user pylint
+pip install --user python-language-server
+pip install --user r7insight_python
+pip install --user redis
+pip install --user requests_mock
+pip install --user rope
+pip install --user rtv
+pip install --user scipy
+pip install --user scrapy
+pip install --user seaborn
+pip install --user selenium
+pip install --user speedtest-cli
+pip install --user streamlink
+pip install --user tldr
+pip install --user trash-cli
+pip install --user pygments
+pip install --user truffleHog
+pip install --user virtualenv
+pip install --user virtualenvwrapper
+pip install --user yapf
 
 rm -fr get-pip.py
 
 #pip list --user | cut -d" " -f 1 | tail -n +3 | xargs pip install -U --user
 notify "Generating ~/.pylintrc"
-pylint --generate-rcfile > ~/.pylintrc
+pylint --generate-rcfile >~/.pylintrc
 
 breakLine
