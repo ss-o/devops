c6c751a59b5b8884ce3ecb03a2d5be73c48efcc8
Salvydas Lukosius <sall@w-ss.io>
2020-12-12 13:12:57 +0000

    test

---
+++ archlinux/typescript.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+CDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "${CDIR}/../utils.sh"
+
+if _cmd_ npm; then
+    title "Installing typescript"
+    npm install -g typescript
+    breakLine
+else
+    notify "NPM required to install typescript"
+fi
