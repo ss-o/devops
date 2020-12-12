34cbbd7ce85e2fc0e0cf49930bbdbf13bcf245ff
Salvydas Lukosius <sall@w-ss.io>
2020-12-12 10:14:00 +0000

    rbenv

--- rbenv.sh
+++ rbenv.sh
@@ -16,7 +16,7 @@ sudo apt install -y git curl autoconf bison build-essential \
     libssl-dev libyaml-dev libreadline6-dev zlib1g-dev \
     libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev
 
-sudo rm -rf ~/.rbenv/
+sudo rm -rf ~/.rbenv
 notify "Cloning rbenv"
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 source ~/.bashrc
