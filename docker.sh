4a15084771a2ebdb9d45281cd6b47c3826f79cbf
Salvydas Lukosius <sall@w-ss.io>
2020-12-12 09:07:28 +0000

    modified:   docker.sh

--- docker.sh
+++ docker.sh
@@ -7,5 +7,9 @@ title "Installing docker"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo bash get-docker.sh
 sudo rm get-docker.sh
+notify "Configuring and enabling docker"
 sudo usermod -aG docker ${USER}
+sudo systemctl enable docker
+sudo systemctl start docker
+
 breakLine
