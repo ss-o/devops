1939ded9b175dca8e7a3c7daf1d47f5d66e0b965
Salvydas Lukosius <sall@w-ss.io>
2020-12-12 08:47:03 +0000

    modified:   ruby.sh

--- ruby.sh
+++ ruby.sh
@@ -4,6 +4,8 @@ source "${CDIR}/utils.sh"
 
 title "Installing Ruby with DAPP v${versionDapp}"
 sudo apt install -y ruby-dev gcc pkg-config
+sudo chown -R ${USER} /var/lib/gems
+sudo chown -R ${USER} /usr/local/bin
 sudo gem install mixlib-cli -v 1.7.0
 sudo gem install dapp -v ${versionDapp}
 breakLine
