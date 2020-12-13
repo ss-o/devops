58b4b35e7b845a96f2259e5decdc8e8882241d0f
Salvydas Lukosius <sall@w-ss.io>
2020-12-13 05:28:44 +0000

    modified:   utils.sh

--- python.sh
+++ python.sh
@@ -2,36 +2,29 @@
 CDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${CDIR}/utils.sh"
 
-if _exec_ apt; then
-       . "deb_base/deps-devops.sh"
-fi
-if _exec_ pacman; then
-       . "archlinux/deps.sh"
-fi
-
-title "Installing Python" && echo
-
-mkdir -p ${HOME}/.local/bin
+[[ ! -d "$HOME/.local" ]] && mkdir -p "$HOME/.local"
 source ~/.bashrc
 
-git clone https://github.com/ss-o/cpython.git
-cd cpython
-git checkout v${versionPython}
-./configure --enable-optimizations --with-ensurepip=install
-make -j 4
+curlToFile "https://www.python.org/ftp/python/3.9.1/Python-${versionPython}.tar.xz" "python.tar.xz"
+tar xvf python.tar.xz
+
+cd Python-${versionPython}
+./configure --with-ensurepip=install --prefix=/usr
+make
 sudo make altinstall
 cd - >/dev/null 2>&1
-sudo rm -r cpython
 
-python3 -m pip install --upgrade pip setuptools wheel
+sudo rm -r Python-${versionPython} python.tar.xz
+
+python3 -m pip3 install --user --upgrade pip setuptools wheel
 
 notify "Installing PIP" && echo
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 ${PWD}/get-pip.py --prefix="$HOME/.local"
+python3 ${PWD}/get-pip.py --user
 
 notify "Installing Tools using PIP" && echo
-pip install --upgrade pip
-pip install autopep8
+pip install --user --upgrade pip
+pip install --user autopep8
 pip install --user black
 pip install --user cheat
 pip install --user django
@@ -41,30 +34,26 @@ pip install --user httpie
 pip install --user importmagic
 pip install --user jupyter
 pip install --user litecli
-pip install matplotlib
+pip install --user matplotlib
 pip install --user pandas
-pip install pipenv
+pip install --user pipenv
 pip install --user poetry
 pip install --user progressbar2
 pip install --user psycopg2-binary
 pip install --user py-spy
 pip install --user pydoc_utils
 pip install --user pyflakes
-pip install pylint
+pip install --user pylint
 pip install --user python-language-server
 pip install --user tldr
 pip install --user conda
 pip install --user pygments
-pip install virtualenv
-pip install virtualenvwrapper
-pip install yapf
+pip install --user virtualenv
+pip install --user virtualenvwrapper
+pip install --user yapf
 
 rm -fr get-pip.py
 
 #pip list --user | cut -d" " -f 1 | tail -n +3 | xargs pip install -U --user
-#notify "Generating ~/.pylintrc" && echo
-#pylint --generate-rcfile > ~/.pylintrc
 
 breakLine
-
-exec "$SHELL"
