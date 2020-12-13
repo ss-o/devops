3dd6f4800c13a3b0f97584d3ee49a9ed113e6f61
Salvydas Lukosius <sall@w-ss.io>
2020-12-13 03:16:37 +0000

    new file:   .vscode/settings.json 	deleted:    get-pip.py

--- python-tools.sh
+++ python-tools.sh
@@ -9,7 +9,6 @@ if _exec_ pacman; then
        . "archlinux/deps.sh"
 fi
 
-
 title "Installing Python" && echo
 
 mkdir -p ${HOME}/.local/bin
@@ -28,37 +27,37 @@ python3 -m pip install --upgrade pip setuptools wheel
 
 notify "Installing PIP" && echo
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 ${PWD}/get-pip.py --user --prefix="$HOME/.local"
+python3 ${PWD}/get-pip.py --prefix="$HOME/.local"
 
 notify "Installing Tools using PIP" && echo
-pip install --user --upgrade pip
-pip install --user autopep8
-#pip install --user black
-#pip install --user cheat
-#pip install --user django
-#pip install --user faker
-#pip install --user flake8
-#pip install --user httpie
-#pip install --user importmagic
-#pip install --user jupyter
-#pip install --user litecli
-#pip install --user matplotlib
-#pip install --user pandas
-pip install --user pipenv
-#pip install --user poetry
-#pip install --user progressbar2
-#pip install --user psycopg2-binary
-#pip install --user py-spy
-#pip install --user pydoc_utils
-#pip install --user pyflakes
-#pip install --user pylint
-#pip install --user python-language-server
-#pip install --user tldr
-#pip install --user conda
-#pip install --user pygments
-pip install --user virtualenv
-pip install --user virtualenvwrapper
-#pip install --user yapf
+pip install --upgrade pip
+pip install autopep8
+pip install --user black
+pip install --user cheat
+pip install --user django
+pip install --user faker
+pip install --user flake8
+pip install --user httpie
+pip install --user importmagic
+pip install --user jupyter
+pip install --user litecli
+pip install matplotlib
+pip install --user pandas
+pip install pipenv
+pip install --user poetry
+pip install --user progressbar2
+pip install --user psycopg2-binary
+pip install --user py-spy
+pip install --user pydoc_utils
+pip install --user pyflakes
+pip install pylint
+pip install --user python-language-server
+pip install --user tldr
+pip install --user conda
+pip install --user pygments
+pip install virtualenv
+pip install virtualenvwrapper
+pip install yapf
 
 rm -fr get-pip.py
 
