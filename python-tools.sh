1f55c737a98e1f17f3eab503e6d278d327c31882
Salvydas Lukosius <sall@w-ss.io>
2020-12-13 02:38:22 +0000

    new file:   get-pip.py

--- python-tools.sh
+++ python-tools.sh
@@ -19,52 +19,53 @@ source ~/.bashrc
 git clone https://github.com/ss-o/cpython.git
 cd cpython
 git checkout v${versionPython}
-./configure --prefix=$HOME/.local
-make
-sudo make install
+./configure --enable-optimizations --with-ensurepip=install
+make -j 4
+sudo make altinstall
 cd - >/dev/null 2>&1
 sudo rm -r cpython
+
+python3 -m pip install --upgrade pip setuptools wheel
 
 notify "Installing PIP" && echo
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 ${PWD}/get-pip.py --user --prefix="$HOME/.local"
-python3 -m pip install --upgrade pip setuptools wheel
 
 notify "Installing Tools using PIP" && echo
 pip install --user --upgrade pip
 pip install --user autopep8
-pip install --user black
-pip install --user cheat
-pip install --user django
-pip install --user faker
-pip install --user flake8
-pip install --user httpie
-pip install --user importmagic
-pip install --user jupyter
-pip install --user litecli
-pip install --user matplotlib
-pip install --user pandas
-pip install --user pipenv
-pip install --user poetry
-pip install --user progressbar2
-pip install --user psycopg2-binary
-pip install --user py-spy
-pip install --user pydoc_utils
-pip install --user pyflakes
-pip install --user pylint
-pip install --user python-language-server
-pip install --user tldr
-pip install --user conda
-pip install --user pygments
+#pip install --user black
+#pip install --user cheat
+#pip install --user django
+#pip install --user faker
+#pip install --user flake8
+#pip install --user httpie
+#pip install --user importmagic
+#pip install --user jupyter
+#pip install --user litecli
+#pip install --user matplotlib
+#pip install --user pandas
+#pip install --user pipenv
+#pip install --user poetry
+#pip install --user progressbar2
+#pip install --user psycopg2-binary
+#pip install --user py-spy
+#pip install --user pydoc_utils
+#pip install --user pyflakes
+#pip install --user pylint
+#pip install --user python-language-server
+#pip install --user tldr
+#pip install --user conda
+#pip install --user pygments
 pip install --user virtualenv
 pip install --user virtualenvwrapper
-pip install --user yapf
+#pip install --user yapf
 
 rm -fr get-pip.py
 
 #pip list --user | cut -d" " -f 1 | tail -n +3 | xargs pip install -U --user
-notify "Generating ~/.pylintrc" && echo
-pylint --generate-rcfile > ~/.pylintrc
+#notify "Generating ~/.pylintrc" && echo
+#pylint --generate-rcfile > ~/.pylintrc
 
 breakLine
 
