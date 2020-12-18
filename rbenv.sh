15541b6656df2457f098c7d92636e04e9ec43e2b
Salvydas Lukosius <sall@w-ss.io>
2020-12-18 03:21:30 +0000

    fix

--- rbenv.sh
+++ rbenv.sh
@@ -11,35 +11,35 @@ source "${CDIR}/lib/utils.sh"
 if _cmd_ rbenv; then
     notify "rbenv already install installed, want to reinstall?" && echo
     if _confirm; then
-        return
+
+        title "Installing rbenv" && echo
+        sudo rm -rf ~/.rbenv
+
+        notify "Cloning rbenv" && echo
+        git clone https://github.com/Digital-Clouds/rbenv.git ~/.rbenv
+
+        notify "Cloning ruby-build" && echo
+        git clone https://github.com/Digital-Clouds/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+        _source_bashrc
+
+        notify "Installing Ruby" && echo
+        rbenv install ${versionRuby} #Installing required version of Ruby
+        rbenv global ${versionRuby}
+        rbenv use ${versionRuby}
+        rbenv rehash
+
+        notify "Gem installing tools" && echo
+        gem install bundler rdoc rails mixlib-cli dapp
+
+        notify "To verify that rbenv is properly set up, running rbenv doctor" && echo
+        sleep 2
+
+        curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
+
     else
         exit
     fi
 fi
-
-title "Installing rbenv" && echo
-sudo rm -rf ~/.rbenv
-
-notify "Cloning rbenv" && echo
-git clone https://github.com/Digital-Clouds/rbenv.git ~/.rbenv
-
-notify "Cloning ruby-build" && echo
-git clone https://github.com/Digital-Clouds/ruby-build.git ~/.rbenv/plugins/ruby-build
-
-_source_bashrc
-
-notify "Installing Ruby" && echo
-rbenv install ${versionRuby} #Installing required version of Ruby
-rbenv global ${versionRuby}
-rbenv use ${versionRuby}
-rbenv rehash
-
-notify "Gem installing tools" && echo
-gem install bundler rdoc rails mixlib-cli dapp
-
-notify "To verify that rbenv is properly set up, running rbenv doctor" && echo
-sleep 2
-
-curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
 
 breakLine
