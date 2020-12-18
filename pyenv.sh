dcd2255f36bfeb7bb7582ea133f6ab5bf0a9a07a
Salvydas Lukosius <sall@w-ss.io>
2020-12-18 02:51:45 +0000

    fix

--- pyenv.sh
+++ pyenv.sh
@@ -74,7 +74,7 @@ pip install virtualenvwrapper
 pip install yapf
 pip install thefuck
 
-_source_bashrc
+exec "$SHELL"
 
 #pip list --user | cut -d" " -f 1 | tail -n +3 | xargs pip install -U --user
 
